55b5377ba7bbd1f7cd1f42af70a0d0c7d9bdcbd5
Victor Lima <victor.lima@devsquad.com>
2022-10-08 00:15:47 -0300

    installing laravel and dependencies

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<head>
+    <title>WireChess</title>
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    @livewireScripts
+</body>
